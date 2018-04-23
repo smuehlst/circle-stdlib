0e6c7d50ba4904cc2e8e15467f689350011e1637
R. Stange <rsta2@o2online.de>
2018-04-23 10:54:42 +0200

    class CHTTPClient added
    Supports both HTTP and HTTPS requests
    
    samples/mbedtls/06-webclient added (ported from Circle)

---
+++ include/circle-mbedtls/httpclient.h
@@ -0,0 +1,70 @@
+//
+// httpclient.h
+//
+// Circle - A C++ bare metal environment for Raspberry Pi
+// Copyright (C) 2017-2018  R. Stange <rsta2@o2online.de>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#ifndef _circle_mbedtls_httpclient_h
+#define _circle_mbedtls_httpclient_h
+
+#include <circle/net/netsubsystem.h>
+#include <circle/net/ipaddress.h>
+#include <circle-mbedtls/http.h>
+#include <circle/net/netsocket.h>
+#include <circle/string.h>
+#include <circle/types.h>
+
+namespace CircleMbedTLS {
+
+class CHTTPClient
+{
+public:
+	CHTTPClient (CNetSubSystem *pNetSubSystem,
+		     CIPAddress	   &rServerIP,
+		     u16	    nServerPort = HTTP_PORT,
+		     const char	   *pServerName = 0,		// required for virtual and HTTPS servers
+		     boolean	    bUseSSL     = FALSE);	// HTTPS?
+	~CHTTPClient (void);
+
+	THTTPStatus Get (const char *pPath,			// "/file[?name=value[&name=value...]]"
+			 u8	    *pBuffer,			// content will be returned here
+			 unsigned   *pLength);			// in: buffer size, out: content length
+
+	THTTPStatus Post (const char *pPath,			// "/file[?name=value[&name=value...]]"
+			  u8	     *pBuffer,			// content will be returned here
+			  unsigned   *pLength,			// in: buffer size, out: content length
+			  const char *pFormData);		// "name=value[&name=value...]"
+
+private:
+	THTTPStatus Request (THTTPRequestMethod	 Method,
+			     const char		*pPath,		// may include URL parameters
+			     u8			*pBuffer,	// content will be returned here
+			     unsigned		*pLength,	// in: buffer size, out: content length
+			     const char		*pFormData = 0);// form data for POST or 0
+
+private:
+	CNetSubSystem *m_pNetSubSystem;
+	CIPAddress     m_ServerIP;
+	u16	       m_ServerPort;
+	CString	       m_ServerName;
+	boolean	       m_bUseSSL;
+
+	CNetSocket    *m_pSocket;
+};
+
+}
+
+#endif
