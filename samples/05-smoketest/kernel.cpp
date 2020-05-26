bff20c7c4d47aa98ec6e68426f2242a7a7bf0c0d
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2020-05-26 21:07:57 +0200

    mkdir() and unlink() implemented
    Update to new newlib version that has unlink() and mkdir()
    support.
    Extended smoke test with tests for unlink() and mkdir().

--- samples/05-smoketest/kernel.cpp
+++ samples/05-smoketest/kernel.cpp
@@ -24,6 +24,11 @@
 #include <cstring>
 #include <cerrno>
 #include <dirent.h>
+#include <string>
+#include <sys/stat.h>
+#include <unistd.h>
+
+using namespace std;
 
 namespace
 {
@@ -92,138 +97,174 @@ CKernel::IoTest (void)
 {
     Report ("Test file operations...");
 
-    char const stdio_filename[] = "stdio.txt";
+    string const stdio_filename = "stdio.txt";
 
-    FILE *fp = fopen (stdio_filename, "w");
+    FILE *fp = fopen (stdio_filename.c_str (), "w");
 
-    if (fp != nullptr)
+    if (fp == nullptr)
     {
-        fprintf (fp, "Opened file with (FILE *) %p\n", fp);
-        fclose (fp);
-
-        fp = fopen (stdio_filename, "r");
-        if (fp != nullptr)
-        {
-            Report ("fopen () test succeeded");
-
-            char buf[200];
-            char *p;
-
-            while ((p = fgets (buf, sizeof(buf), fp)) != nullptr)
-            {
-                size_t const len = strlen (buf);
-                if (len > 0 && buf[len - 1] == '\n')
-                {
-                    buf[len - 1] = 0;
-                }
-                printf ("Read from file: '%s'\n", p);
-            }
-
-            // Read the blank after "Opened" by seeking
-            if (fseek (fp, 6L, SEEK_SET) != 0)
-            {
-                PErrorExit ("fseek failed");
-            }
-
-            Report ("fseek () test succeeded");
-
-            char c;
-            if (fread (&c, 1, 1, fp) != 1)
-            {
-                PErrorExit ("fread failed");
-            }
-
-            Report ("fread () test succeeded");
-
-            if (c != ' ')
-            {
-                fprintf (stderr, "Bad read result, expected ' ', got '%c'\n", c);
-                exit (1);
-            }
-
-            Report ("fseek () test succeeded");
-
-            if (fclose (fp) != 0)
-            {
-                PErrorExit ("fclose failed");
-            }
-
-            Report ("fclose () test succeeded");
-        }
-        else
-        {
-            PErrorExit ("Cannot open file for reading with fopen()");
-        }
+        PErrorExit ("Cannot open file for writing with fopen ()");
     }
-    else
+
+    fprintf (fp, "Opened file with (FILE *) %p\n", fp);
+    fclose (fp);
+
+    fp = fopen (stdio_filename.c_str (), "r");
+    if (fp == nullptr)
     {
-        PErrorExit ("Cannot open file for writing with fopen()");
+         PErrorExit ("Cannot open file for reading with fopen()");
     }
+
+    Report ("fopen () test succeeded");
+
+    char buf[200];
+    char *p;
+
+    while ((p = fgets (buf, sizeof(buf), fp)) != nullptr)
+    {
+        size_t const len = strlen (buf);
+        if (len > 0 && buf[len - 1] == '\n')
+        {
+            buf[len - 1] = 0;
+        }
+        printf ("Read from file: '%s'\n", p);
+    }
+
+    // Read the blank after "Opened" by seeking
+    if (fseek (fp, 6L, SEEK_SET) != 0)
+    {
+        PErrorExit ("fseek () failed");
+    }
+
+    Report ("fseek () test succeeded");
+
+    char c;
+    if (fread (&c, 1, 1, fp) != 1)
+    {
+        PErrorExit ("fread failed");
+    }
+
+    Report ("fread () test succeeded");
+
+    if (c != ' ')
+    {
+        fprintf (stderr, "Bad read result, expected ' ', got '%c'\n", c);
+        exit (1);
+    }
+
+    Report ("fseek () test succeeded");
+
+    if (fclose (fp) != 0)
+    {
+        PErrorExit ("fclose () failed");
+    }
+
+    Report ("fclose () test succeeded");
 
     Report ("Test directory operations...");
 
-    DIR * const dir = opendir ("/");
-    if (dir != nullptr)
+    string const dirname = "subdir";
+
+    if (mkdir (("/" + dirname).c_str(), 0) != 0)
     {
-        Report ("opendir () test succeeded");
-
-        while (true)
-        {
-            errno = 0;
-            struct dirent const *const dp = readdir (dir);
-            if (dp != nullptr)
-            {
-                printf ("\t%s\n", dp->d_name);
-            }
-            else
-            {
-                if (errno != 0 && errno != ENOENT)
-                {
-                    PErrorExit ("readdir () failed");
-                }
-
-                break;
-            }
-        }
-
-        Report ("readir () test succeeded");
-
-        printf ("Rewinding directory...\n");
-        rewinddir (dir);
-
-        Report ("rewinddir () test succeeded");
-
-        printf ("Listing \".\" directory with readdir_r:\n");
-        while (true)
-        {
-            struct dirent de;
-            struct dirent *dep;
-            int error = readdir_r (dir, &de, &dep);
-            if (error != 0)
-            {
-                PErrorExit ("readdir_r () failed");
-            }
-            if (dep != nullptr)
-            {
-                printf ("\t%s\n", dep->d_name);
-            }
-            else
-            {
-                break;
-            }
-        }
-        printf ("Closing directory...\n");
-        if (closedir (dir) != 0)
-        {
-            PErrorExit ("closedir() failed ");
-        }
-
-        Report ("closedir () test succeeded");
+        PErrorExit ("mkdir () failed");
     }
-    else
+
+    Report ("mkdir () test succeeded");
+
+    DIR * const dir = opendir ("/");
+    if (dir == nullptr)
     {
         PErrorExit ("opendir (\"/\") failed");
     }
+
+    Report ("opendir () test succeeded");
+
+    bool found = false;
+    while (true)
+    {
+        errno = 0;
+        struct dirent const *const dp = readdir (dir);
+        if (dp != nullptr)
+        {
+            printf ("\t%s\n", dp->d_name);
+            found = found || dp->d_name == dirname;
+        }
+        else
+        {
+            if (errno != 0 && errno != ENOENT)
+            {
+                PErrorExit ("readdir () failed");
+            }
+
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        PErrorExit (("Did not find directory '" + dirname + "'").c_str ());
+    }
+
+    Report ("readdir () test succeeded");
+
+    printf ("Rewinding directory...\n");
+    rewinddir (dir);
+
+    Report ("rewinddir () test succeeded");
+
+    printf ("Listing \".\" directory with readdir_r:\n");
+    while (true)
+    {
+        struct dirent de;
+        struct dirent *dep;
+        int error = readdir_r (dir, &de, &dep);
+        if (error != 0)
+        {
+            PErrorExit ("readdir_r () failed");
+        }
+        if (dep != nullptr)
+        {
+            printf ("\t%s\n", dep->d_name);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    printf ("Closing directory...\n");
+    if (closedir (dir) != 0)
+    {
+        PErrorExit ("closedir() failed ");
+    }
+
+    Report ("closedir () test succeeded");
+
+    string const subdir_filename = "/" + dirname + "/file.txt";
+
+    fp = fopen (subdir_filename.c_str (), "w");
+    if (fp == nullptr)
+    {
+        PErrorExit ("fopen () failed");
+    }
+
+    if (fprintf (fp, "bla bla") < 0)
+    {
+        PErrorExit ("fprintf () failed");
+    }
+
+    if (fclose (fp) != 0)
+    {
+        PErrorExit ("fclose () failed");
+    }
+
+    if (unlink (subdir_filename.c_str ()) < 0)
+    {
+        PErrorExit ("unlink () failed");
+    }
+
+    Report ("unlink () test succeeded");
 }
 
 void
