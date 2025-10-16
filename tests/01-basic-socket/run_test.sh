2fdf16e893714022505dfc3707d0f29ec5641db1
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-10-16 20:41:34 +0200

    Avoid error because of kill in script

--- tests/01-basic-socket/run_test.sh
+++ tests/01-basic-socket/run_test.sh
@@ -14,5 +14,8 @@ $CFG_QEMU_BIN -M $CFG_QEMU_MACHINE $CFG_QEMU_OPT $CFG_QEMU_IMG \
     -display none -nographic -semihosting -sd sdcard.img \
     -netdev "user,id=net0,hostfwd=tcp::5000-:5000,guestfwd=tcp:10.0.2.100:5001-cmd:yes simulated server" \
     -device usb-net,netdev=net0
+exit_code=$?
 
 kill $server_pid 2>/dev/null || true
+
+exit $exit_code
