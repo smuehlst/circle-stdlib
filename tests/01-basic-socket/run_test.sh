d5b328c59023ccb5b0e1d7dbcc95db8722504a05
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-10-17 19:17:32 +0200

    Undo accidental commit of wrong script

--- tests/01-basic-socket/run_test.sh
+++ tests/01-basic-socket/run_test.sh
@@ -5,14 +5,14 @@ set -e
 ../../testdata/setup-sd.sh
 make
 
-ulimit -t 240
+ulimit -t 120
 
 ./socket_test.sh &
 server_pid=$!
 
 $CFG_QEMU_BIN -M $CFG_QEMU_MACHINE $CFG_QEMU_OPT $CFG_QEMU_IMG \
     -display none -nographic -semihosting -sd sdcard.img \
-    -netdev "user,id=net0,hostfwd=tcp::5000-:5000,guestfwd=tcp:10.0.2.100:5001-cmd:yes simulated server" \
+    -netdev "user,id=net0,hostfwd=tcp::5000-:5000,guestfwd=tcp:10.0.2.100:5001-cmd:/bin/echo simulated server" \
     -device usb-net,netdev=net0
 exit_code=$?
 
