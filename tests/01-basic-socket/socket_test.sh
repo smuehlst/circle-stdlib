13330ef32dca18a4ffdfd8eb32e9522d8e256a8f
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-10-18 16:10:00 +0200

    Workaround attempt for hangs with Intel QEMU

--- tests/01-basic-socket/socket_test.sh
+++ tests/01-basic-socket/socket_test.sh
@@ -6,6 +6,8 @@ while ! nc -z localhost 5000; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
+sleep 10  # ensure listen() is active
+
 echo "Socket test" | nc -v localhost 5000
 
 echo "Socket test completed"
