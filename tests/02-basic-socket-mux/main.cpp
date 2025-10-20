51c07ed6457ca6de457de38f0c8768d4539aaaf3
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2025-10-20 22:22:53 +0200

    Added basic test for poll()

--- tests/02-basic-socket-mux/main.cpp
+++ tests/02-basic-socket-mux/main.cpp
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/select.h>
+#include <poll.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
@@ -94,6 +95,87 @@ TEST_CASE("Basic select read/write test")
             else
             {
                 printf("Port %d: connection closed\n", START_PORT + i);
+            }
+        }
+    }
+
+    for (auto const &sock : socks)
+    {
+        REQUIRE(close(sock) >= 0);
+    }
+}
+
+TEST_CASE("Basic poll read/write test")
+{
+    constexpr int START_PORT = 5000;
+    constexpr size_t NUM_PORTS = 3;
+    int socks[NUM_PORTS];
+
+    for (auto &sock : socks)
+    {
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        REQUIRE(sock >= 0);
+
+        uint32_t const simulated_server_ip = (10U << 24) | (0U << 16) | (2U << 8) | 2U;
+
+        struct sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_addr.s_addr = htonl(simulated_server_ip);
+        server_address.sin_port = htons(START_PORT + (&sock - socks));
+
+        REQUIRE(connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) >= 0);
+    }
+
+    // Monitor all sockets for writability using poll()
+    struct pollfd fds[NUM_PORTS];
+    for (size_t i = 0; i < NUM_PORTS; ++i)
+    {
+        fds[i].fd = socks[i];
+        fds[i].events = POLLOUT;
+        fds[i].revents = 0;
+    }
+
+    int ready = poll(fds, NUM_PORTS, 5000); // 5 second timeout
+    REQUIRE(ready > 0);
+
+    for (size_t i = 0; i < NUM_PORTS; ++i)
+    {
+        if (fds[i].revents & POLLOUT)
+        {
+            char msg[64];
+            snprintf(msg, sizeof(msg), "Hello from client on port %d\n", START_PORT + (int)i);
+            ssize_t const sent = write(fds[i].fd, msg, strlen(msg));
+            REQUIRE(sent >= 0);
+            printf("Sent to port %d: %s", START_PORT + (int)i, msg);
+        }
+    }
+
+    // --- Now: Wait for readability and read replies using poll() ---
+    for (size_t i = 0; i < NUM_PORTS; ++i)
+    {
+        fds[i].fd = socks[i];
+        fds[i].events = POLLIN;
+        fds[i].revents = 0;
+    }
+
+    ready = poll(fds, NUM_PORTS, 5000);
+    REQUIRE(ready > 0);
+
+    char buf[128];
+    for (size_t i = 0; i < NUM_PORTS; ++i)
+    {
+        if (fds[i].revents & POLLIN)
+        {
+            ssize_t const n = read(fds[i].fd, buf, sizeof(buf) - 1);
+            REQUIRE(n >= 0);
+            if (n > 0)
+            {
+                buf[n] = '\0';
+                printf("Reply from port %d: %s", START_PORT + (int)i, buf);
+            }
+            else
+            {
+                printf("Port %d: connection closed\n", START_PORT + (int)i);
             }
         }
     }
