e9d9c5633dcb285e525ae118550b21511e490ceb
Stephan Mühlstrasser <stephan.muehlstrasser@web.de>
2026-01-01 23:28:52 +0100

    Adapt test to new timing behavior
    Circle commit 5d80ee2ac5c1a940d0557f9e11db87b75e70c6f3 changed the
    timing behavior of the network code which made the test fail because
    of a delay loop in the test shell script.

--- tests/01-basic-socket/socket_test.sh
+++ tests/01-basic-socket/socket_test.sh
@@ -2,10 +2,6 @@
 
 echo "Waiting for QEMU to open socket 5000..."
 
-while ! nc -z localhost 5000; do   
-  sleep 0.1 # wait for 1/10 of the second before check again
-done
-
 sleep 10  # ensure listen() is active
 
 echo "Socket test" | nc -v localhost 5000
